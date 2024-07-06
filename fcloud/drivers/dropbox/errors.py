b58a86d127b9d58aa4babadad10874e133df32d6
Artem <theakopov@proton.me>
2024-07-06 13:27:29 +0300

    Dropbox: errors are printed  correctly

--- fcloud/drivers/dropbox/errors.py
+++ fcloud/drivers/dropbox/errors.py
@@ -1,20 +1,26 @@
+from textwrap import dedent
+
 from ...exceptions.driver_errors import DriverError
-from ...exceptions.driver_exceptions import DriverException
+from ...exceptions.exceptions import DriverException
 
 
 class DropboxError(DriverError):
     auth_error = (
         "Auth error",
-        """Maybe the connection data is specified incorrectly 
+        dedent(
+            """\
+        Maybe the connection data is specified incorrectly
         or is missing from the configuration file. It is also 
         possible that the token has expired.
         More: http://fcloud.tech/docs/clouds/dropbox/connect/""",
+        ),
     )
 
     perrmission_denied_error = (
         "Permission denied",
-        """Perhaps you have not granted permissions to the file.
-        More: http://fcloud.tech/docs/clouds/dropbox/errors/""",
+        dedent("""\
+        Perhaps you have not granted permissions to the file.
+        More: http://fcloud.tech/docs/clouds/dropbox/errors/"""),
     )
 
     invalid_token_error = (
@@ -29,14 +35,16 @@ class DropboxError(DriverError):
 
     validation_error = (
         "Validation error",
-        """The data entered does not fit the template: '(/(.|[\r\n])*)?|id:.*|(ns:[0-9]+(/.*)?).
-        Here is an example of a folder path: /films/2024""",
+        dedent("""\
+        The data entered does not fit the template: '(/(.|[\r\n])*)?|id:.*|(ns:[0-9]+(/.*)?).
+        Here is an example of a folder path: /films/2024"""),
     )
 
     uncorrect_data_error = (
         "Api data validation error",
-        """You may not have filled in the required fields in .conf or you may have entered invalid values.
-        Details: {}""",
+        dedent("""\
+        You may not have filled in the required fields in .conf or you may have entered invalid values.
+        Details: {}"""),
     )
 
     badinput_error = (
